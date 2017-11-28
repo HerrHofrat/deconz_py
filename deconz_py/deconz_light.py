56c7d71cb47fb6d0de98c75f785dab035eaefae5
abmantis <amfcalt@gmail.com>
2017-11-28 23:20:13 +0000

    fix light (was not working) and simplify some code

--- deconz_py/deconz_light.py
+++ deconz_py/deconz_light.py
@@ -15,76 +15,35 @@ class DeCONZLight:
 
         self._dcz_id = dcz_id
         self._name = name
-        self._state = state
         self._device_type = device_type
         self._api = api
-        group = False
-        if self._device_type == self.COLOR_TEMPERATURE_LIGHT:
-            current_state = self._state['on']
-            reachable = self._state['reachable']
-            dimmer = self._state['bri']
-            ct_color = self._state['ct']
-        elif self._device_type == self.DIMMABLE_LIGHT:
-            current_state = self._state['on']
-            reachable = self._state['reachable']
-            dimmer = self._state['bri']
-        elif self._device_type == self.EXTENDED_COLOR_LIGHT:
-            current_state = self._state['on']
-            reachable = True
-            dimmer = self._state['bri']
-            ct_color = self._state['ct']
-            group = True
-        elif self._device_type == self.LIGHT_GROUP:
-            current_state = self._state['on']
-            reachable = self._state['reachable']
-            dimmer = self._state['bri']
-            xy_color = self._state['xy']
 
-        self._current_state = current_state
-        self._reachable = reachable
-        self._dimmer = dimmer
-        self._is_group = group
-        self._xy_color = xy_color
-        self._ct_color = ct_color
+        self._is_group = self._device_type == self.LIGHT_GROUP
+
+        # set defaults. the values will be updated on "parse_state()"
+        self._current_state = False
+        self._dimmer = None
+        self._ct_color = None
+        self._xy_color = None
+        self._reachable = False
+
+        self.parse_state(state)
+
         self._update_listeners = []
-
 
     @asyncio.coroutine
     def update(self, data):
         """Update the state of the device."""
         if 'state' in data:
-            if 'on' in data['state'] and (self._device_type == self.DIMMABLE_LIGHT or
-                                          self._device_type == self.COLOR_TEMPERATURE_LIGHT):
-                self._current_state = data['state']['on']
-            elif 'any_on' in data['state'] and self._device_type == self.LIGHT_GROUP:
-                self._current_state = data['state']['any_on']
-            elif 'bri' in data['state']:
-                self.brightness = data['state']['bri']
-            elif 'ct' in data['state']:
-                self.color_temp = data['state']['ct']
-            #elif 'x' in data:
-            #    color_util.color_xy_brightness_to_RGB(self._xy_color[0], 1, 0)
-            #    self._xy_color = data['x'],self._xy_color[1]
-            #elif 'y' in data:
-            #    color_util.color_xy_brightness_to_RGB(self._xy_color[0], 1, 0)
-            #    self._xy_color = self._xy_color[0],data['y']
+            self.parse_state(data['state'])
 
         for update_listener in self._update_listeners:
-            yield from update_listener()
-
-    def add_update_listener(self, update_listener):
-        """update_listener is called as soon as the sensor receives an update"""
-        self._update_listeners.append(update_listener)
-
-    def remove_update_listener(self, update_listener):
-        """remove an update_listener"""
-        self._update_listeners.remove(update_listener)
+            yield from update_listener(data)
 
     @property
     def xy_color(self):
         """Return the XY color value."""
         return self._xy_color
-
 
     @xy_color.setter
     def xy_color(self, value):
@@ -123,15 +82,14 @@ class DeCONZLight:
         """Return the is_group of the light."""
         return self._is_group
 
-
     @property
     def is_on(self):
         """Return true if light is on."""
         return self._current_state
 
     @property
-    def device_class(self):
-        """The device class."""
+    def device_type(self):
+        """The device type."""
         return self._device_type
 
     @asyncio.coroutine
@@ -145,3 +103,35 @@ class DeCONZLight:
         """Instruct the light to turn on."""
         self._current_state = True
         yield from self._api.set_light(self.is_on, self)
+
+    def parse_state(self, state):
+
+        if 'on' in state:
+            self._current_state = state['on']
+        elif 'any_on' in state:
+            self._current_state = state['any_on']
+
+        if 'bri' in state:
+            self._dimmer = state['bri']
+        if 'ct' in state:
+            self._ct_color = state['ct']
+        if 'xy' in state:
+            self._xy_color = state['xy']
+
+        if 'reachable' in state:
+            self._reachable = state['reachable']
+
+        # elif 'x' in data:
+        #    color_util.color_xy_brightness_to_RGB(self._xy_color[0], 1, 0)
+        #    self._xy_color = data['x'],self._xy_color[1]
+        # elif 'y' in data:
+        #    color_util.color_xy_brightness_to_RGB(self._xy_color[0], 1, 0)
+        #    self._xy_color = self._xy_color[0],data['y']
+
+    def add_update_listener(self, update_listener):
+        """update_listener is called as soon as the sensor receives an update"""
+        self._update_listeners.append(update_listener)
+
+    def remove_update_listener(self, update_listener):
+        """remove an update_listener"""
+        self._update_listeners.remove(update_listener)
